f7177b6fbb176cf9e19833eb8cc05556e8b7cbeb
Matthew Storm <llamanade1127@gmail.com>
2026-02-09 17:32:48 -0600

    Added Security Package

---
+++ BLELocalEventManager/app/src/main/java/edu/uwm/cs595/goup11/backed/security/Crypto.kt
@@ -0,0 +1,21 @@
+package edu.uwm.cs595.goup11.backed.security
+
+/**
+ * Handles all data encryption/decryption and
+ */
+object Crypto {
+
+    fun generateMAC() {
+
+    }
+
+    fun authenticateMAC() {
+
+    }
+
+    fun encryptMessage() {
+
+    }
+
+
+}
