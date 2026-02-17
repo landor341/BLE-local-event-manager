bccb54230f590831975491bfab7ea5eb604c9a49
AngeloGiampetroni <giampet3@uwm.edu>
2026-02-17 13:54:07 -0600

    Add function stubs and imports Add imports for encryption setup and function stubs

--- BLELocalEventManager/app/src/main/java/edu/uwm/cs595/goup11/backend/security/Crypto.kt
+++ BLELocalEventManager/app/src/main/java/edu/uwm/cs595/goup11/backend/security/Crypto.kt
@@ -1,21 +1,58 @@
 package edu.uwm.cs595.goup11.backend.security
 
+import java.security.SeccureRandom
+import java.util.UUID
+import java.util.zip.CRC32
+import javax.crypto.Cipher
+import javax.crypto.Mac
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
 /**
- * Handles all data encryption/decryption and
+ * Handles all data encryption/decryption, ID generation, and integrity checks
  */
 object Crypto {
 
-    fun generateMAC() {
+    /**
+     * Generates a unique UUID
+     */
+    fun generateUUID(): String {
 
     }
 
-    fun authenticateMAC() {
+    /**
+     * Calculates a CRC32 checksum for a redundant data check
+     */
+    fun calculateChecksum(data: ByteArray): Long {
 
     }
 
-    fun encryptMessage() {
+    /**
+     * Generates a HMAC using SHA-256
+     */
+    fun generateMAC(data: ByteArray, key: ByteArray): ByteArray {
 
     }
 
+    /**
+     * Checks if provided MAC matches data using provided key
+     */
+    fun authenticateMAC(data: ByteArray, macToVerify: ByteArray, key: ByteArray): Boolean {
 
+    }
+
+    /**
+     * Encrypts a message using AES in CBC mode with PKCS5 padding
+     * The Initialization Vector (IV) is prepended to the ciphertext
+     */
+    fun encryptMessage(data: ByteArray, key: ByteArray): ByteArray {
+
+    }
+
+    /**
+     * Decrypts a message that was encrypted using encryptMessage
+     */
+    fun decryptMessage(encryptedData: ByteArray, key: ByteArray): ByteArray {
+
+    }
 }
