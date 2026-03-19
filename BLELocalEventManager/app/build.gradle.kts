bfecfe097504a2419d1052012dce0bc79e60303f
chiaoyi <chiaoyi@uwm.edu>
2026-03-19 15:57:27 -0500

    implement PresentationDetailScreen to show detail of  selected presentation

--- BLELocalEventManager/app/build.gradle.kts
+++ BLELocalEventManager/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.core.i18n)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
